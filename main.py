0849bd6ea7f621763f5a8968c6ecf1de16839870
mrehani <mrehani73@gmail.com>
2025-01-11 22:42:14 -0600

    Added params

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import flast module
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request,abort
 from flask_cors import CORS
 
 # instance of flask application
@@ -11,9 +11,15 @@ CORS(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/test")
-def test():
-    data = {'key': 'value'}
+@app.route("/save",methods=["POST"])
+def save():
+    requiredParms = ["cardID","regularQuantity","reverseQuantity","firstEditionQuantity","shadowlessQuantity"]
+    body = request.form.to_dict() 
+    missingParams = [x for x in requiredParms if x not in body.keys()]
+    if (len(missingParams) > 0):
+        abort(400, "Missing the following parameter: "+','.join(missingParams)) 
+    
+    data = {'key': 'vaaaalue'}
     return jsonify(data)
 
 if __name__ == '__main__':  
